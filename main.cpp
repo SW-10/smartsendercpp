8bf69d4da3f8b0afbc251f22a52ba0e77f1a0d2b
teisvh <teisvh@gmail.com>
2023-02-15 11:06:29 +0100

    First commit

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 //using namespace config4cpp;
 
 int main() {
-    config4cpp::Configuration *  cfg = config4cpp::Configuration::create();
-    const char *     scope = "";
-    const char *     configFile = "moby.cfg";
-    const char *     inputFile;
-    cfg->parse(configFile);
-    inputFile = cfg->lookupString(scope, "inputfile");
-    std::cout << "Hello, World!" << inputFile << std::endl;
+    // config4cpp::Configuration *  cfg = config4cpp::Configuration::create();
+    // const char *     scope = "";
+    // const char *     configFile = "moby.cfg";
+    // const char *     inputFile;
+    // cfg->parse(configFile);
+    // inputFile = cfg->lookupString(scope, "inputfile");
+    // std::cout << "Hello, World!" << inputFile << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
