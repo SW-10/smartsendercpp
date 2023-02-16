e6de61d3788a7d58b06c176673f1aab5b0865496
karljohan12 <43264510+karljohan12@users.noreply.github.com>
2023-02-16 10:41:59 +0100

    Config (#5)
    * configfile work now
    
    * config reconfigure

--- src/config.h
+++ src/config.h
@@ -2,13 +2,35 @@
 // Created by power on 15-02-2023.
 //
 
+
 #ifndef MOBYCPP_CONFIG_H
 #define MOBYCPP_CONFIG_H
 
-
-class config {
-
+#include <string>
+#include <cstring>
+#include "vector"
+struct columns {
+    int col;
+    float error;
+    int isAbsolute;
 };
+
+class configParameters {
+public:
+
+    columns latCol, longCol;
+    std::vector<columns> cols;
+    std::vector<int> text_cols;
+    int timestampCol;
+    int numberOfCols = 0;
+    int number_of_text_cols;
+    std::string outPutCsvFile;
+    int containsPosition;
+    char* output;
+    configParameters(int argc, char *argv[]);
+    void column_or_text(int* count, char* token);
+};
+
 
 
 #endif //MOBYCPP_CONFIG_H
