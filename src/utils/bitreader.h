87b3a3bebc4f17106f6e26684d1e151fd668a932
teisvh <teisvh@gmail.com>
2023-02-17 12:08:41 +0100

    Fixed bitreader.h

--- src/utils/bitreader.h
+++ src/utils/bitreader.h
@@ -9,3 +9,9 @@ struct BitReader{
     int bytesCount;
     std::vector<uint8_t> bytes;
 }typedef BitReader;
+
+BitReader tryNewBitreader(std::vector<uint8_t> bytes, int byteCount);
+uint32_t read_bits(BitReader* bitReader, uint8_t numberOfBits);
+uint32_t read_bit(BitReader* bitReader);
+float intToFloat(uint32_t value);
+#endif
