d6f94f8f1839e58bec2f1d62c4211cdfce0526c0
Emil Laurits Bech <e.bech@icloud.com>
2023-02-28 12:39:52 +0100

    Remove text

--- src/managers/ModelManager.cpp
+++ src/managers/ModelManager.cpp
@@ -114,3 +114,32 @@ TextModelContainer::TextModelContainer(int id) {
     this->id = id;
     this->reCheck = true;
 }
+
+/*TEST_CASE("Same values return 1"){
+
+  TextCompressor TextCompressor(1);
+
+  std::vector<int> checks;
+  std::vector<int> timestamps = {0, 10,20,30,39,48,55,62,69,76};
+  for(int i = 0; i < timestamps.size(); i++){
+    checks.push_back(TextCompressor.fitString("STATUS:OK",timestamps[i]));
+  };
+  std::vector<int> comparison = {1,1,1,1,1,1,1,1,1,1};
+
+  CHECK_EQ(checks, comparison);
+
+}
+
+TEST_CASE("Different values return 0"){
+  TextCompressor TextCompressor(1);
+
+  std::vector<int> checks;
+  std::vector<int> timestamps = {0, 10,20,30,39,48,55,62,69,76};
+  for(int i = 0; i < timestamps.size(); i++){
+    checks.push_back(TextCompressor.fitString(("STATUS:" + std::to_string(i)),timestamps[i]));
+  };
+  std::vector<int> comparison = {1,0,0,0,0,0,0,0,0,0};
+
+  CHECK_EQ(checks, comparison);
+
+}*/
