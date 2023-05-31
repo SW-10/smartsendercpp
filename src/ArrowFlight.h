f6a4b7387f3489cd0df5024b6be89c4e4abf30ce
agneborn98 <43062786+agneborn98@users.noreply.github.com>
2023-05-31 10:18:16 +0200

    No arrow on unix

--- src/ArrowFlight.h
+++ src/ArrowFlight.h
@@ -1,33 +1,33 @@
-#pragma once
-
-#include <string>
-#include "arrow/flight/api.h"
-#include "arrow/api.h"
-#include "arrow/flight/sql/api.h"
-#include "managers/ModelManager.h"
-
-namespace flight = arrow::flight;
-namespace flightsql = arrow::flight::sql;
-
-struct ConnectionAddress {
-    ConnectionAddress(std::string host, int32_t port);
-
-    std::string host;
-    int32_t port;
-};
-
-arrow::Result<std::shared_ptr<arrow::Table>> VectorToColumnarTable(
-        const std::vector<struct SelectedModel>& rows);
-
-arrow::Result<std::shared_ptr<arrow::RecordBatch>> MakeRecordBatch(const
-                                                                   std::shared_ptr<arrow::Table>& table);
-
-arrow::Status WriteRecordBatchVectorToParquet(
-        const std::vector<std::shared_ptr<arrow::RecordBatch>> &table,
-        std::string pathToFile);
-
-arrow::Result<std::unique_ptr<flight::FlightClient>>
-createClient(const ConnectionAddress &address);
-
-arrow::Result<std::unique_ptr<flightsql::FlightSqlClient>>
-createSqlClient(std::unique_ptr<flight::FlightClient> flightClient);
+//#pragma once
+//
+//#include <string>
+//#include "arrow/flight/api.h"
+//#include "arrow/api.h"
+//#include "arrow/flight/sql/api.h"
+//#include "managers/ModelManager.h"
+//
+//namespace flight = arrow::flight;
+//namespace flightsql = arrow::flight::sql;
+//
+//struct ConnectionAddress {
+//    ConnectionAddress(std::string host, int32_t port);
+//
+//    std::string host;
+//    int32_t port;
+//};
+//
+//arrow::Result<std::shared_ptr<arrow::Table>> VectorToColumnarTable(
+//        const std::vector<struct SelectedModel>& rows);
+//
+//arrow::Result<std::shared_ptr<arrow::RecordBatch>> MakeRecordBatch(const
+//                                                                   std::shared_ptr<arrow::Table>& table);
+//
+//arrow::Status WriteRecordBatchVectorToParquet(
+//        const std::vector<std::shared_ptr<arrow::RecordBatch>> &table,
+//        std::string pathToFile);
+//
+//arrow::Result<std::unique_ptr<flight::FlightClient>>
+//createClient(const ConnectionAddress &address);
+//
+//arrow::Result<std::unique_ptr<flightsql::FlightSqlClient>>
+//createSqlClient(std::unique_ptr<flight::FlightClient> flightClient);
